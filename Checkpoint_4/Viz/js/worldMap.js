0b0534767efb611a8fdd51305ef9ac9c0d31bcdc
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2019-11-26 16:15:32 +0000

    added the report for checkpoint 4

--- Checkpoint_4/Viz/js/worldMap.js
+++ Checkpoint_4/Viz/js/worldMap.js
@@ -1,12 +1,20 @@
 var dataset;
 
 d3.csv("../VI_datasets/Air_pollution_exposure.csv").then(function (data) {
-    console.log(data);
+    //console.log(data);
     //dataset = data;
 
     //gen_map_vis();
 });
 
 function gen_map_vis() {
+    var h = 800;
+    var w = 1200;
 
+    var svg = d3.select("#worldMap")
+        .append("svg")
+        .attr("height", h)
+        .attr("width", w);
+
+    var path = d3.geoPath();
 }
